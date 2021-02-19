9962388122b678ec7da681630f21d4677b77e09a
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-02-19 16:56:58 +0300

    but fix all project

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-let buttonEditProfile = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let buttonEditProfile = document.querySelector('.profile__edit-button');
 let buttonClosePopup = document.querySelector('.popup__close');
 let formPopup = document.querySelector('.popup__form');
 let profileTitleName = document.querySelector('.profile__title');
@@ -8,27 +8,33 @@ let formNameInput = document.querySelector('.popup__input-name');
 let formJobInput = document.querySelector('.popup__input-job');
 let cardLikeActive = document.querySelectorAll('.card__like');
 
-cardLikeActive.forEach(function(elem) {
-  elem.addEventListener('click', function () {
-    this.classList.toggle('card__like_active');
-  });
-});
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
 
-buttonEditProfile.addEventListener('click', function () {
-  popup.classList.toggle("popup_opened");
-  formNameInput.value = profileTitleName.textContent;
-  formJobInput.value = profileSubtitleName.textContent;
-});
-
-buttonClosePopup.addEventListener('click', function () {
-  popup.classList.toggle("popup_opened");
-});
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitleName.textContent = formNameInput.value;
   profileSubtitleName.textContent = formJobInput.value;
-  popup.classList.toggle("popup_opened");
+  closePopup();
 }
 
+buttonEditProfile.addEventListener('click', function () {
+  formNameInput.value = profileTitleName.textContent;
+  formJobInput.value = profileSubtitleName.textContent;
+  openPopup();
+});
+
+buttonClosePopup.addEventListener('click', closePopup);
+
 formPopup.addEventListener('submit', formSubmitHandler);
+
+cardLikeActive.forEach(function(elem) {
+  elem.addEventListener('click', function () {
+    this.classList.toggle('card__like_active');
+  });
+});
